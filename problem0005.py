00ff3001fce2411451dc6a3738a964f4c79d5f6f
KQ <eternal.september@gmail.com>
2016-06-16 11:42:13 -0400

    still working on problem 5...

--- problem0005.py
+++ problem0005.py
@@ -9,20 +9,35 @@
 
 def isDivisibleByEachNumber(number):
 	""" A function that can check through a list of divisors. """
-	for x in range (1, 11):
+	for x in range (11, 21):
 		if (number % x != 0):
+			print("Failed at {0}".format(x))
 			return False
 	return True
 
+"""
+11
+12 -> divisible by 2, 3, 4, 6
+13
+14 -> divisible by 2, 7
+15 -> divisible by 3, 5
+16 -> divisible by 2, 4, 8
+17
+18 -> divisible by 2, 3, 6, 9
+19
+20 -> divisible by 2, 4, 5, 10
+"""
 
 initNum = 2520
-maxNum = 1
+maxProduct = 1
+for x in range(11,21):
+	maxProduct *= x
 
-for x in range(11, 21):
-	maxNum *= x
+result = isDivisibleByEachNumber(maxProduct)
+print("product: {0}, result: {1}".format(maxProduct, result))
 
-print (maxNum)
-
-check = isDivisibleByEachNumber(2520)
-print(check)
-
+'''
+product = initNum * 11 * 13 * 17 * 19
+result = isDivisibleByEachNumber(product)
+print("product: {0}, result: {1}".format(product, result))
+'''
