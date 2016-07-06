6c933d8be7a15bbae542207e1990f2d2644cc87f
KQ <eternal.september@gmail.com>
2016-07-06 10:12:06 -0400

    solved problem 9

--- problem0009.py
+++ problem0009.py
@@ -9,3 +9,29 @@
 # Find the product a*b*c.
 
 
+def findPythagoreanTriple(totalSum):
+	""" 
+	Takes the sum of a + b + c as parameter. 
+	Implements Euclid's formula.
+	"""
+	upperRange = totalSum // 2
+
+	for m in range(2, upperRange):
+		# If m > n, then n's max is m - 1
+		# But range doesn't include the last number.
+		for n in range(1, m):
+			# initial values for (3,4,5) triplet:
+			# m = 2, n = 1
+			a = m*m - n*n 
+			b = 2*m*n 
+			c = m*m + n*n
+
+			if (a+b+c == totalSum):
+				product = a*b*c
+				return [[a, b, c], product]
+
+
+
+
+[triplet, product] = findPythagoreanTriple(1000)
+print("Triplet: {0}. Product: {1}".format(triplet, product))
